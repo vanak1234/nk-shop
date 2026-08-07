d64a990e35de0b299cadc5ac4c9c54650353d79f
Morb Vanak <vanakmorb@gmail.com>
2026-08-07 13:35:16 +0700

    update

--- src/components/common/Banner.jsx
+++ src/components/common/Banner.jsx
@@ -9,20 +9,7 @@ const Banner = () => {
   {/* Dark Overlay (Optional) */}
   <div className="absolute inset-0 bg-black/40"></div>
 
-  {/* Text */}
-  <div className="relative z-10 flex flex-col justify-center h-full px-10 md:px-20 text-white">
-    <h1 className="text-4xl md:text-6xl font-bold">
-      Welcome to Fishing Store
-    </h1>
-
-    <p className="mt-4 text-lg md:text-2xl max-w-xl">
-      Premium fishing equipment for every adventure.
-    </p>
-
-    <button className="mt-8 w-fit bg-blue-600 hover:bg-blue-700 px-6 py-3 rounded-lg font-semibold">
-      Shop Now
-    </button>
-  </div>
+  
 </div>
   )
 }
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -11,7 +11,26 @@ const Home = () => {
 
             <Banner/>
           </div>
-
+          <div className="main-card container mx-auto py-5 ">
+            <div className="card lg:w-[23%] md:w-[48%] sm:w-[48%] h-90 border border-gray-200 shadow rounded-2xl">
+                <div className="card-img h-[60%]">
+                  <img src="" alt="" />
+                </div>
+                <div className="body px-5 h-[40%]">
+                   <p>category</p>
+                   <h1>title</h1>
+                   <p>star</p>
+                   <div className="price-cart flex justify-between items-center  ">
+                     <div className="price">
+                      <h1>100$</h1>
+                     </div>
+                     <div className="cart">
+                      <button><i class="ri-shopping-bag-4-fill"></i></button>
+                     </div>
+                   </div>
+                </div>
+            </div>
+          </div>
         </div>
 
     </div>
